07c0afdfcd32fba92446faf5098aec8c6705a5ad
Elliott Arnold <si3mshady@gmail.com>
2023-05-26 10:51:31 -0500

    save point

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 # Define your IP address
 variable "keypair" {
   description = "ssh-key"
-  default     = "sreuniversity"  # Replace with your actual IP address
+  default     = "sreuniversity"  # Replace with your actual keypair name
 }
 
 # Define your IP address
@@ -27,6 +27,19 @@ resource "aws_vpc" "vpc_us_east_1" {
 resource "aws_vpc" "vpc_us_east_2" {
   cidr_block = "10.2.0.0/16"  # Modified CIDR block
   provider   = aws.us-east-2
+}
+
+# Create internet gateways
+resource "aws_internet_gateway" "igw_us_east_2" {
+  vpc_id     = aws_vpc.vpc_us_east_2.id
+  provider   = aws.us-east-2
+}
+
+# Attach internet gateway to VPC
+resource "aws_vpc_gateway_attachment" "attachment_us_east_2" {
+  vpc_id             = aws_vpc.vpc_us_east_2.id
+  internet_gateway_id = aws_internet_gateway.igw_us_east_2.id
+  provider           = aws.us-east-2
 }
 
 # Create subnets in each VPC
@@ -54,6 +67,23 @@ resource "aws_subnet" "subnet_us_east_2b" {
   cidr_block        = "10.2.2.0/24"  # Modified CIDR block
   availability_zone = "us-east-2b"
   provider          = aws.us-east-2
+}
+
+# Create load balancers in each region
+resource "aws_lb" "load_balancer_us_east_1" {
+  name               = "lb-us-east-1"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.security_group_us_east_1.id]
+  subnets            = [aws_subnet.subnet_us_east_1a.id, aws_subnet.subnet_us_east_1b.id]
+}
+
+resource "aws_lb" "load_balancer_us_east_2" {
+  name               = "lb-us-east-2"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.security_group_us_east_2.id]
+  subnets            = [aws_subnet.subnet_us_east_2a.id, aws_subnet.subnet_us_east_2b.id]
 }
 
 # Create security groups for the instances and load balancers
@@ -100,34 +130,21 @@ resource "aws_security_group" "security_group_us_east_2" {
 
 # Create instances in each region
 resource "aws_instance" "instance_us_east_1" {
-  ami           = "ami-053b0d53c279acc90"  # Replace with the actual AMI ID
-  instance_type = "t2.medium"
-  key_name      = var.keypair
-  subnet_id     = aws_subnet.subnet_us_east_1a.id
-  vpc_security_group_ids = [aws_security_group.security_group_us_east_1.id]
+  provider                  = aws
+  ami                       = "ami-053b0d53c279acc90"
+  instance_type             = "t2.micro"
+  key_name                  = var.keypair
+  subnet_id                 = aws_subnet.subnet_us_east_1a.id
+  vpc_security_group_ids    = [aws_security_group.security_group_us_east_1.id]
+  associate_public_ip_address = true
 }
 
 resource "aws_instance" "instance_us_east_2" {
-  ami           = "ami-024e6efaf93d85776"  # Replace with the actual AMI ID
-  instance_type = "t2.medium"
-  key_name      = var.keypair
-  subnet_id     = aws_subnet.subnet_us_east_2a.id
-  vpc_security_group_ids = [aws_security_group.security_group_us_east_2.id]
-}
-
-# Create load balancers in each region
-resource "aws_lb" "load_balancer_us_east_1" {
-  name               = "lb-us-east-1"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.security_group_us_east_1.id]
-  subnets            = [aws_subnet.subnet_us_east_1a.id, aws_subnet.subnet_us_east_1b.id]
-}
-
-resource "aws_lb" "load_balancer_us_east_2" {
-  name               = "lb-us-east-2"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.security_group_us_east_2.id]
-  subnets            = [aws_subnet.subnet_us_east_2a.id, aws_subnet.subnet_us_east_2b.id]
+  provider                  = aws.us-east-2
+  ami                       = "ami-024e6efaf93d85776"
+  instance_type             = "t2.micro"
+  key_name                  = var.keypair
+  subnet_id                 = aws_subnet.subnet_us_east_2a.id
+  vpc_security_group_ids    = [aws_security_group.security_group_us_east_2.id]
+  associate_public_ip_address = true
 }
