3d0af5f9d2c5316004c7e1a5bd1f3c7d3c8ddc7f
Elliott Arnold <si3mshady@gmail.com>
2023-05-26 09:56:36 -0500

    save point

--- flask_app.py
+++ flask_app.py
@@ -16,5 +16,24 @@ def get_instance_info():
         'Region': region
     })
 
+
+@app.route("/instance")
+def get_instance_info():
+    instance_ip = requests.get('http://169.254.169.254/latest/meta-data/public-ipv4').text
+    instance_id = requests.get('http://169.254.169.254/latest/meta-data/instance-id').text
+    region = requests.get('http://169.254.169.254/latest/meta-data/placement/availability-zone').text[:-1]
+    
+    return jsonify({
+        'Instance IP': instance_ip,
+        'Instance ID': instance_id,
+        'Region': region
+    })
+
+
+@app.route("/health")
+def get_instance_info():
+    return jsonify({"status": "ok"}), 200
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
